25dc03a735ae206ccd37b3d8584d2545a4e6c192
Jonathan Harris <46660273+JonathanHarris15@users.noreply.github.com>
2022-07-27 21:39:53 -0500

    added the first version of the drive function. Probably does not work

--- PRL.c
+++ PRL.c
@@ -1,5 +1,39 @@
-void drive(){
+#DEFINE right_wheel 0
+#DEFINE left_wheel 1
+#DEFINE wheel_circumference 21.99115
+#DEFINE distance_between_wheels 16.3
+#DEFINE pi 3.14159
 
+int right_wheel_tpr = 1000;
+int left_wheel_tpr = 1000;
+
+void set_wheel_ticks(int left, int right){
+    left_wheel_tpr = left;
+    right_wheel_tpr = right;
+}
+
+void drive(float distance, int speed){
+    if(speed > 1400){
+        printf("RUNTIME ERROR: drive speed must not exceed 1400\n Now stopping the program");
+        shut_down_in(1);
+        return;
+    }
+    int right_wheel_target_ticks = (right_wheel_tpr/wheel_circumference) * distance;
+    int left_wheel_target_ticks = (left_wheel_tpr/wheel_circumference) * distance;
+    float tps = (1.0981818181818(speed) – 5.6363636363636);
+    float spt = 1/tps;
+    float seconds_to_completion = right_wheel_target_ticks * spt;
+    int right_wheel_target_speed =  speed;
+    int left_wheel_target_speed = ((left_wheel_target_ticks/seconds_to_completion) - 5.6363636363636)/1.0981818181818;
+    cmpc(right_wheel);
+    cmpc(left_wheel);
+    while(gmpc(right_wheel) < right_wheel_target_ticks || gmpc(left_wheel) < left_wheel_target_ticks){
+        mav(right_wheel, right_wheel_target_speed);
+        mav(left_wheel, left_wheel_target_speed);
+    }
+    mav(right_wheel, 0);
+    mav(left_wheel, 0);
+    msleep(50);
 }
 void right(){
 

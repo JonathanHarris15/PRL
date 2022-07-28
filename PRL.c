1fcd7f5e44ea412bc320b6e9b32c3e4bd4548216
Jonathan Harris <46660273+JonathanHarris15@users.noreply.github.com>
2022-07-27 22:18:00 -0500

    fixed bugs

--- PRL.c
+++ PRL.c
@@ -1,8 +1,8 @@
-#DEFINE right_wheel 0
-#DEFINE left_wheel 1
-#DEFINE wheel_circumference 21.99115
-#DEFINE distance_between_wheels 16.3
-#DEFINE pi 3.14159
+#define right_wheel 0
+#define left_wheel 1
+#define wheel_circumference 21.99115
+#define distance_between_wheels 16.3
+#define pi 3.14159
 
 int right_wheel_tpr = 1000;
 int left_wheel_tpr = 1000;
@@ -20,11 +20,13 @@ void drive(float distance, int speed){
     }
     int right_wheel_target_ticks = (right_wheel_tpr/wheel_circumference) * distance;
     int left_wheel_target_ticks = (left_wheel_tpr/wheel_circumference) * distance;
-    float tps = (1.0981818181818(speed) - 5.6363636363636);
+    float tps = ((1.0981818181818 * speed) - 5.6363636363636);
     float spt = 1/tps;
     float seconds_to_completion = right_wheel_target_ticks * spt;
-    int right_wheel_target_speed =  speed;
+    int right_wheel_target_speed =  ((right_wheel_target_ticks/seconds_to_completion) - 5.6363636363636)/1.0981818181818;
     int left_wheel_target_speed = ((left_wheel_target_ticks/seconds_to_completion) - 5.6363636363636)/1.0981818181818;
+    printf("right: %d\n",right_wheel_target_speed);
+    printf("left: %d\n",left_wheel_target_speed);
     cmpc(right_wheel);
     cmpc(left_wheel);
     while(gmpc(right_wheel) < right_wheel_target_ticks || gmpc(left_wheel) < left_wheel_target_ticks){

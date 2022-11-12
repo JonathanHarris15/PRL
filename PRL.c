6a4351171f7ae38f34267a88fe19f529310b196e
Jonathan Harris <46660273+JonathanHarris15@users.noreply.github.com>
2022-11-11 23:44:42 -0600

    Progress Baby! Demo side is looking clean and the ticks are working for the create (I think). I also implimented the master functions that either call from the demo or roomba depending on whether or not the roomba was set up.

--- PRL.c
+++ PRL.c
@@ -1,45 +1,133 @@
 /*
-PRL v0.5
+PRL v0.56
 Creator: Jonathan Harris, Jacob Ross
 Advisors: Zach Zimmerman, Nathan Povendo, Qbit
 the Plainview Robotics Library is the entire collection of commands used by the Plainview Robotics Team.
 These functions rely on the internal counters of the wheels to make commands as accurate as possible so 
 being as accurate as possible in the setup is important.
 */
-
-
-#define right_wheel 0
-#define left_wheel 1
-#define wheel_circumference 21.36283
-#define distance_between_wheels 16.25
 #define pi 3.14159265359
 
-//speed to ticks_per_second = (1.08 * abs(speed)) - 2
+int create_in_use = 0;
+
+float wheel_circumference 21.36283;
+float distance_between_wheels 16.25;
+float right_wheel_tpr = 1000;
+float left_wheel_tpr = 1000;
+float right_wheel_tpc = 0;
+float left_wheel_tpc = 0;
+
 
 float grey_value = 1700;
 float minimum_line_follow_radius = 30;
 float maximum_line_follow_radius = 1000;
-int right_wheel_tpr = 1000;
-int left_wheel_tpr = 1000;
-float right_wheel_tpc = 0;
-float left_wheel_tpc = 0;
+
 float accel_distance = 2;
 float accel_deg = 10;
 
+//change the distance that the robot accels and deccels
+void set_accel_window_drive(float distance){
+    accel_distance = distance;
+}
+
+//change the angle that the robot accels and deccels
+void set_accel_window_turn(float degrees){
+    accel_deg = degrees;
+}
+
+//function that takes the error value received from the line follow sensor and gives back a radius for the turn
+float line_follow_calculate_radius(int error_value, float max_radius, float min_radius){
+    //NOTE: white means positive error and black means negative error FOR LEFT SIDE LINE FOLLOW
+    float error_modifier = 1500;
+    if(error_value > error_modifier){
+        return -min_radius;
+    }else if(error_value < -error_modifier){
+        return min_radius;
+    }else if(error_value < 0){
+        return (error_value*((max_radius-min_radius)/error_modifier)) + max_radius;
+    }else{
+        return (error_value*((max_radius-min_radius)/error_modifier)) - max_radius;
+    }
+}
+
+//takes the speed of the origin and get the individual wheel speeds
+float * calculate_wheel_speed(float radius, float speed){
+    static float speeds[2];
+    float theta = speed/radius;
+    speeds[0] = theta * (radius-(distance_between_wheels*0.5));
+    speeds[1] = theta * (radius+(distance_between_wheels*0.5));
+    return speeds;
+}
+
+//servo function by Jacob of Noble High School (written on August 8,2019)
+void servo(int port, int position, int speed){   
+    int current = get_servo_position(port);
+    if(position > 2047){
+        position = 2047;
+    } 
+    if(position < 0){
+        position = 0;
+    }   
+    while(current <= position-speed || current >= position+speed){ 
+        if(current < position){
+            current += speed; 
+        }
+        if(current > position){
+            current -= speed;
+        }
+        set_servo_position(port, current);
+        msleep(2);
+    }
+    set_servo_position(port,position); 
+} 
+
+void drive(float distance, int speed){
+    if(create_in_use = 0){
+        d_drive(distance, speed);
+    }else{
+        r_drive(distance, speed);
+    }
+}
+
+void line_follow(float distance, int speed, int port){
+    if(create_in_use = 0){
+        d_line_follow(distance, speed, port);
+    }else{
+        r_line_follow(distance, speed, port);
+    }
+}
+
+void turn_right(float degree, float speed, double radius){
+    if(create_in_use = 0){
+        d_turn_right(degree, speed, radius);
+    }else{
+        r_line_right(degree, speed, radius);
+    }
+}
+
+void turn_left(float degree, float speed, double radius){
+    if(create_in_use = 0){
+        d_turn_right(degree, speed, radius);
+    }else{
+        r_line_right(degree, speed, radius);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//DEMO FUNCTIONS
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#define right_wheel 0
+#define left_wheel 1
+//speed to ticks_per_second = (1.08 * abs(speed)) - 2
 
 void set_wheel_ticks(int left, int right){
     left_wheel_tpr = left;
     right_wheel_tpr = right;
     left_wheel_tpc = left/wheel_circumference;
     right_wheel_tpc = right/wheel_circumference;
-}
-
-void set_accel_window_drive(float distance){
-    accel_distance = distance;
-}
-
-void set_accel_window_turn(float degrees){
-    accel_deg = degrees;
 }
 
 void spin_motor(int port, int ticks, int speed){
@@ -51,8 +139,7 @@ void spin_motor(int port, int ticks, int speed){
     msleep(20);
 }
 
-void drive(float distance, int speed){
-    
+void d_drive(float distance, int speed){
     int right_wheel_target_ticks = (right_wheel_tpr/wheel_circumference) * distance;
     int left_wheel_target_ticks = (left_wheel_tpr/wheel_circumference) * distance;
     float accel_window_ticks = (right_wheel_target_ticks/wheel_circumference) * accel_distance;
@@ -105,28 +192,7 @@ void drive(float distance, int speed){
 }
 
 
-float line_follow_calculate_radius(int error_value, float max_radius, float min_radius){
-    float error_modifier = 1500; //NOTE: white means positive error and black means negative error FOR LEFT SIDE LINE FOLLOW
-    if(error_value > error_modifier){
-        return -min_radius;
-    }else if(error_value < -error_modifier){
-        return min_radius;
-    }else if(error_value < 0){
-        return (error_value*((max_radius-min_radius)/error_modifier)) + max_radius;
-    }else{
-        return (error_value*((max_radius-min_radius)/error_modifier)) - max_radius;
-    }
-}
-
-float * calculate_wheel_speed(float radius, float speed){
-    static float speeds[2];
-    float theta = speed/radius;
-    speeds[0] = theta * (radius-(distance_between_wheels*0.5));
-    speeds[1] = theta * (radius+(distance_between_wheels*0.5));
-    return speeds;
-}
-
-void line_follow(float distance, float speed, int port){
+void d_line_follow(float distance, float speed, int port){
     cmpc(0);
    	cmpc(1);
     int prev_right_ticks = 0;
@@ -191,7 +257,7 @@ void line_follow(float distance, float speed, int port){
     msleep(50);
 }
 
-void right_turn(float degree, float speed, double radius){
+void d_right_turn(float degree, float speed, double radius){
     
     double right_radius = radius-distance_between_wheels/2;
     double left_radius = radius+distance_between_wheels/2;
@@ -224,9 +290,6 @@ void right_turn(float degree, float speed, double radius){
         mav(right_wheel, right_speed*speed_modifier);
         mav(left_wheel, left_speed*speed_modifier);
         msleep(5);
-        //double left_theta =  ((gmpc(left_wheel)/left_wheel_tpc)/(left_radius))*57.29577951;
-        //double right_theta =  ((gmpc(right_wheel)/right_wheel_tpc)/(right_radius))*57.29577951;
-        //theta = (right_theta+left_theta)/2;
         if(abs(right_radius) > abs(left_radius)){
           	theta = ((gmpc(right_wheel)/right_wheel_tpc)/(right_radius))*57.29577951;
         }else{
@@ -239,7 +302,7 @@ void right_turn(float degree, float speed, double radius){
     msleep(20);
     
 }
-void left_turn(float degree, float speed, double radius){
+void d_left_turn(float degree, float speed, double radius){
     
     double right_radius = radius+distance_between_wheels/2;
     double left_radius = radius-distance_between_wheels/2;
@@ -272,9 +335,6 @@ void left_turn(float degree, float speed, double radius){
         mav(right_wheel, right_speed*speed_modifier);
         mav(left_wheel, left_speed*speed_modifier);
         msleep(5);
-        //double left_theta =  ((gmpc(left_wheel)/left_wheel_tpc)/(left_radius))*57.29577951;
-        //double right_theta =  ((gmpc(right_wheel)/right_wheel_tpc)/(right_radius))*57.29577951;
-        //theta = (right_theta+left_theta)/2;
         if(abs(right_radius) > abs(left_radius)){
           	theta = ((gmpc(right_wheel)/right_wheel_tpc)/(right_radius))*57.29577951;
         }else{
@@ -287,39 +347,53 @@ void left_turn(float degree, float speed, double radius){
     msleep(20);
     
 }
-//servo function by Jacob of Noble High School (written on August 8,2019)
-void servo(int port, int position, int speed){   
 
-    int current = get_servo_position(port);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    if(position > 2047){
+//CREATE FUNCTIONS
 
-        position = 2047;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    } 
-    if(position < 0){
+void create_activate(){
+    create_in_use = 1;
+    create_connect();
+    wheel_circumference 22.61946711;
+    distance_between_wheels 23.5;
+    left_wheel_tpr = 508.8;
+    right_wheel_tpr = 508.8;
+    left_wheel_tpc = 508.8/wheel_circumference;
+    right_wheel_tpc = 508.8/wheel_circumference;
 
-        position = 0;
+}
 
-    }   
-
-    while(current <= position-speed || current >= position+speed){ 
-
-        if(current < position){
-
-            current += speed;
-           
-        }
-        if(current > position){
-
-            current -= speed;
-
-        }
-        set_servo_position(port, current);
-        msleep(2);
+int create_gmec(char wheel){
+    if(wheel == 'r' || wheel == 'R'){
+        create_write_byte(142);
+		create_write_byte(101);
+		char buffer[28];
+		create_read_block(buffer, sizeof (buffer));
+		return (buffer[2] << 8) | (buffer[3] << 0);
+    }else if(wheel == 'l' || wheel == 'L'){
+        create_write_byte(142);
+		create_write_byte(101);
+		char buffer[28];
+		create_read_block(buffer, sizeof (buffer));
+		return (buffer[0] << 8) | (buffer[1] << 0);
     }
+    return 0;
+}
 
-    set_servo_position(port,position);
+void r_drive(float distance, int speed){
     
-} 
+}
 
+void r_line_follow(float distance, float speed, int port){
+   
+}
+
+void r_right_turn(float degree, float speed, double radius){
+    
+}
+void r_left_turn(float degree, float speed, double radius){
+   
+}
